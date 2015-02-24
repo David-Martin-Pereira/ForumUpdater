c04880c00fdc51e6e90df6748a85b352506f0925
david.martin <david.martin@symanto.net>
2015-02-24 17:49:25 +0100

    pre and post addition of cursors on Cursors.txt

---
+++ ForumUpdater/CursorsTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using NUnit.Framework;
+
+namespace ForumUpdater
+{
+    [TestFixture]
+    class CursorsTest
+    {
+        private const string url= "https://disqus.com/api/3.0/threads/list.json?api_key=ytwDh9f4ndTA027jIPzMAFC0hXdeyEEwOsKjujADneiRl2SUdjUpQ40bEXDMhupa&limit=100";
+        private const string dummyCursor = "../../DummyCursor.txt";
+
+        [Test]
+        public void PrecedingToFirstCursorCorrectlyAddedFirst()
+        {
+
+            var urlLocal = url;
+
+            var streamResult = new StreamReader(WebRequest.Create(urlLocal).GetResponse().GetResponseStream());
+        }
+
+        [Test]
+        public void FollowingToLastCursorCorrectyAddedLast()
+        {
+            var urlLocal = url;
+
+            var streamResult = new StreamReader(WebRequest.Create(urlLocal).GetResponse().GetResponseStream());
+        }
+
+        [Test]
+        public void CorrectReplaceOfAFile()
+        {
+            var testText = "asdifjasdof\nsdjfopisjgojid\nofaisjdpofjasodf\n";
+
+            File.WriteAllText(dummyCursor,testText);
+
+            var previousText = "42345235";
+
+            File.WriteAllText(dummyCursor,previousText+"\n"+ File.ReadAllText(dummyCursor));
+
+            //File.AppendAllLines(dummyCursor,new []{previousText});
+
+            var lastLine = File.ReadAllLines(dummyCursor).Last();
+            var firstLine = File.ReadAllLines(dummyCursor).First();
+
+            Assert.AreEqual("42345235", firstLine);
+            Assert.AreEqual("ofaisjdpofjasodf", lastLine);
+
+        }
+    }
+}
--- ForumUpdater/Program.cs
+++ ForumUpdater/Program.cs
@@ -14,7 +14,10 @@ namespace ForumUpdater
         private const string FileForums = "../../Forums.txt";
         private static string _url = "https://disqus.com/api/3.0/threads/list.json?api_key=ytwDh9f4ndTA027jIPzMAFC0hXdeyEEwOsKjujADneiRl2SUdjUpQ40bEXDMhupa&limit=100";
 
-        private static StreamReader _result;
+
+        private static string firstCursor;
+        private static string lastCursor;
+        private static StreamReader _streamResult;
         private static string _jsonString;
         private static DisqusModel _results;
         private static Dictionary<string, string> _forumDictionary;
@@ -33,13 +36,13 @@ namespace ForumUpdater
 
             if (PrepareDictionary())
             {
-                Console.WriteLine("Load complete. Press any key to continue");
+                Console.WriteLine("Load complete. Press enter key to continue");
                 Console.ReadLine();
                 UpdateForum();
             }
             else
             {
-                Console.WriteLine("Load failed. Press any key to exit");
+                Console.WriteLine("Load failed. Press enter key to exit");
                 Console.ReadLine();
             }
         }
@@ -53,14 +56,17 @@ namespace ForumUpdater
         {
             var result = true;
 
-            var lastCursor = File.ReadAllLines(FileCursors).Last();
+            var cursors = File.ReadAllLines(FileCursors);
 
-            _url += "&cursor=" + lastCursor;
+            firstCursor = cursors.First();
+            lastCursor = cursors.Last();
+
+            _url += "&cursor=" + firstCursor;
 
             // ReSharper disable once AssignNullToNotNullAttribute
-            _result = new StreamReader(WebRequest.Create(_url).GetResponse().GetResponseStream());
+            _streamResult = new StreamReader(WebRequest.Create(_url).GetResponse().GetResponseStream());
 
-            _jsonString = _result.ReadToEnd();
+            _jsonString = _streamResult.ReadToEnd();
 
             _results = JsonConvert.DeserializeObject<DisqusModel>(_jsonString);
 
@@ -89,6 +95,43 @@ namespace ForumUpdater
 
         private static void UpdateForum()
         {
+            //actualización de los cursores hacia atrás
+            while (_results.Cursor.HasPrev)
+            {
+                foreach (var feed in _results.Response)
+                {
+                    if (!_forumDictionary.ContainsKey(feed.Forum))
+                    {
+                        _forumDictionary.Add(feed.Forum, feed.Link);
+
+                        Console.WriteLine(feed.Link + " " + feed.Forum);
+
+                        File.AppendAllLines(FileForums, new[] { FixSemiColon(feed.Link) + ";" + feed.Forum });
+                    }
+
+                }
+
+                //reescritura del nuevo cursor hacia arriba (es previo)
+                File.WriteAllText(FileCursors,_results.Cursor.Id+"\n"+File.ReadAllText(FileCursors));
+
+                //reemplazo del parámetro "cursor"
+                _url = _url.Substring(0, _url.IndexOf("&cursor=", StringComparison.Ordinal));
+
+                _url += "&cursor=" + _results.Cursor.Prev;
+
+                // ReSharper disable once AssignNullToNotNullAttribute
+
+                _streamResult = new StreamReader(WebRequest.Create(_url).GetResponse().GetResponseStream());
+                _results = JsonConvert.DeserializeObject<DisqusModel>(_streamResult.ReadToEnd());
+            }
+
+            //reemplazo del parámetro "cursor"
+            _url = _url.Substring(0, _url.IndexOf("&cursor=", StringComparison.Ordinal));
+
+            _url += "&cursor=" + lastCursor;
+
+
+            //actualización de los cursores hacia delante
             // ReSharper disable once LoopVariableIsNeverChangedInsideLoop
             while (_results.Cursor.HasNext)
             {
@@ -105,18 +148,18 @@ namespace ForumUpdater
                     }
 
                 }
-
+                //añadir nuevo cursor al final (es el siguiente)
                 File.AppendAllLines(FileCursors, new[] { _results.Cursor.Id });
 
-
+                //reemplazo del parámetro "cursor"
                 _url = _url.Substring(0, _url.IndexOf("&cursor=", StringComparison.Ordinal));
 
                 _url += "&cursor=" + _results.Cursor.Next;
 
                 // ReSharper disable once AssignNullToNotNullAttribute
                 
-                _result = new StreamReader(WebRequest.Create(_url).GetResponse().GetResponseStream());
-                _results = JsonConvert.DeserializeObject<DisqusModel>(_result.ReadToEnd());
+                _streamResult = new StreamReader(WebRequest.Create(_url).GetResponse().GetResponseStream());
+                _results = JsonConvert.DeserializeObject<DisqusModel>(_streamResult.ReadToEnd());
             }
         }
         
